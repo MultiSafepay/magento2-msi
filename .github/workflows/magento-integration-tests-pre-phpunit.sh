73a580c6897552876692c7dae60775d443df7b26
Daniel Civit <daniel.civit@multisafepay.com>
2022-09-12 18:35:09 +0200

    PLGMAG2V2-555: Fix integration tests

--- .github/workflows/magento-integration-tests-pre-phpunit.sh
+++ .github/workflows/magento-integration-tests-pre-phpunit.sh
@@ -6,8 +6,26 @@ if [[ $GITHUB_REPOSITORY == *"internal"* ]] ; then
     REPO_SUFFIX="-internal"
 fi
 
-composer config repositories.multisafepay-php-sdk vcs git@github.com:MultiSafepay/php-sdk${REPO_SUFFIX}.git
-composer config repositories.multisafepay-core vcs git@github.com:MultiSafepay/magento2${REPO_SUFFIX}-core.git
+# PHP-SDK
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/php-sdk${REPO_SUFFIX}/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    PHP_SDK_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    PHP_SDK_BRANCH_NAME="master"
+fi
+git clone -b ${PHP_SDK_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/php-sdk${REPO_SUFFIX}.git ./package-source/multisafepay/php-sdk/
+
+
+# MAGENTO 2 CORE MODULE
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-core/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    CORE_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    CORE_MODULE_BRANCH_NAME="master"
+fi
+git clone -b ${CORE_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}-core.git ./package-source/multisafepay/magento2-core/
+
+composer config repositories.multisafepay "path" "package-source/multisafepay/*"
 
 composer config minimum-stability dev
 composer config prefer-stable false
+composer require yireo/magento2-replace-bundled:^4.1 --no-update
+composer update
